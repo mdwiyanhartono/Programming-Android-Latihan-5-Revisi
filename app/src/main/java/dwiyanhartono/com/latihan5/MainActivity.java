eb102959d330122d8a037d4572e2519412600586
dwiyan goerge hook <m.dwiyan.hartono@gmail.com>
2020-10-17 09:46:55 +0700

    Pindah MainActivity ke ActivitySecond

--- app/src/main/java/dwiyanhartono/com/latihan5/MainActivity.java
+++ app/src/main/java/dwiyanhartono/com/latihan5/MainActivity.java
@@ -2,13 +2,26 @@ package dwiyanhartono.com.latihan5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnPindahActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnPindahActivity = findViewById(R.id.button_Main_Submit);
+        btnPindahActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivitySecond.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
